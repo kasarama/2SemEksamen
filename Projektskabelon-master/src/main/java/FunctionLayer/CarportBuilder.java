e1aeae6f2cb3fc81e3e1ac9c2a296b6fc24b9bb0
Mia Simone <cph-md291@cphbusiness.dk>
2020-04-21 10:52:33 +0200

    Mia's 3. commit

--- Projektskabelon-master/src/main/java/FunctionLayer/CarportBuilder.java
+++ Projektskabelon-master/src/main/java/FunctionLayer/CarportBuilder.java
@@ -1,10 +1,84 @@
 package FunctionLayer;
 
 public class CarportBuilder {
+    Carport carport = new Carport();
+
+    // Stolper og Rem
+    public int posts (){
+        int numberOfPosts = 0;
+        int length = carport.getLength();
+        int width = carport.getWidth();
+
+        if (length < 300 && width < 510){
+            numberOfPosts = 4;
+        }
+        if (length < 300 && width > 510){
+            numberOfPosts = 6;
+        }
+        if (length > 300 && width < 510){
+            numberOfPosts = 6;
+        }
+        if (length > 300 && width > 510){
+            numberOfPosts = 9;
+        }
+        return numberOfPosts;
+    }
+    public int rem (){
+        int numberOfRem = 0;
+        int length = carport.getLength();
+        int width = carport.getWidth();
+
+        if (length < 300 && width < 510){
+            numberOfRem = 2;
+        }
+        if (length < 300 && width > 510){
+            numberOfRem = 3;
+        }
+        if (length > 300 && width < 510){
+            numberOfRem = 4;
+        }
+        if (length > 300 && width > 510){
+            numberOfRem = 6;
+        }
+        return numberOfRem;
+    }
+
+    // Bræddebolte
+    public int carriageBolts() {
+        int carriageBolts = posts()*2;
+        return carriageBolts;
+    }
+
+    // Spær
+    //public int
+
+
+    // Firkantskiver
+    // Universalbeslag
+    // Beslagskruer
+    // Hulbånd
+    // Understern- og over sternbrædder
+    // Skruer
+    // Trapezplader
+    // Bundskruer
+    // Tætningsprofil
+    // Vandbræt 360
+    // Vandbræt540
+    // Skruer til vandbræt
+    // Skruer til ydrebeklædning
+    // Skruer til inderbeklædning
+
+ // Skur:
+    // Stolpe
+    // Lås
+    // Hængsel
+    // Lægte
+    // Vinkelbeslag
+    // Beklædning
 
 }
 
-
+/*
  Carport buildCarport(CarportRequest carportRequest){
     Carport carport = new Carport();
     carport.setLength(carportRequest.getLength());
@@ -28,12 +102,8 @@ public class CarportBuilder {
 
     int sideStolper= ((length-(length%300))/300+1)*2;
 
-
-
-
   /*
  przeliczamy jakie czesci sa potrzebne, dodajemy czesc do listy.
   sprawdzamy czy jakies sroby  sie powtarzaja i sprawdzamy ile w sumie potrzebujemy,
    wrzocamy je do listy jako jeden element
    */
-}
