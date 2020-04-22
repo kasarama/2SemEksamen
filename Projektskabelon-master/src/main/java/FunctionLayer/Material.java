04b31a955f3bb6f09971a6bcb12e90df854d7fe1
Mona Jakob Meshal <monajakobmeshal@Monas-MacBook.lan>
2020-04-22 20:52:05 +0200

    material class adeed

---
+++ Projektskabelon-master/src/main/java/FunctionLayer/Material.java
@@ -0,0 +1,15 @@
+package FunctionLayer;
+
+public class Material {
+        private String name;
+        private int size;
+        private String unit;
+        private double price;
+        private String comment;
+
+        public Material(String name, int size) {
+            this.name = name;
+            this.size = size;
+        }
+}
+
