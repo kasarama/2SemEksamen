3ae24d4887857e868f302973ca9008864e5c2303
Mona Jakob Meshal <monajakobmeshal@Monas-MacBook.lan>
2020-04-29 16:07:41 +0200

    ..

---
+++ Projektskabelon-master/src/main/java/FunctionLayer/PitchedRoofCalculator.java
@@ -0,0 +1,28 @@
+package FunctionLayer;
+
+public class PitchedRoofCalculator {
+
+    Carport carport;
+    RoofSizing roofSizing;
+
+    private int roofLength;
+    private int roofHeight;
+
+    //Attributes here...something from carport class?
+    private String tagplade1 = "";
+    private String tagplade2 = "";
+    private String taglægte = "";
+    private String toplægte = "";
+    private String toplægteHolder = "";
+    private String tagstenBindere = "";
+    private String tagstenNakkekroge = "";
+    //Spær/Rem/Skruer??
+
+    //TODO: Methods to calculate how much is needed of each item to build a picthed roof.
+    //call methods from 'RoofSizing class' to calculate Roof height + max 'vinkel på tagryg (spisds tag)'
+    //Ex calculate how many tagplade is needed for a certain length?
+
+    //Work with a fixed length and roof angle for simplicity (according to Slack)
+
+
+}
