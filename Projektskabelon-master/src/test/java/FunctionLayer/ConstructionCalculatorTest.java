7f51d3c5fe6f69c2340384b46e2725985a7f3677
kasarama <magdalena.wawrzak@gmail.com>
2020-04-30 23:06:35 +0200

    ConstructionCalculateorTest fixed

--- Projektskabelon-master/src/test/java/FunctionLayer/ConstructionCalculatorTest.java
+++ Projektskabelon-master/src/test/java/FunctionLayer/ConstructionCalculatorTest.java
@@ -8,12 +8,11 @@ import static org.junit.Assert.*;
 
 public class ConstructionCalculatorTest {
 
-    ConstructionCalculator1 c = new ConstructionCalculator1();
     @Test
     public void posts() {
         int length = 480;
         int width = 510;
-        int result = c.posts(length, width);
+        int result = ConstructionCalculator.posts(length, width);
         int expected = 6;
         assertEquals(expected, result);
     }
@@ -21,7 +20,7 @@ public class ConstructionCalculatorTest {
     public void posts2() {
         int length = 480;
         int width = 610;
-        int result = c.posts(length, width);
+        int result = ConstructionCalculator.posts(length, width);
         int expected = 6;
         assertEquals(expected, result);
     }
