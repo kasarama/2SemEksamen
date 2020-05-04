bb54990b6831c96790226f2a59921a5cc638af12
Cathrine C <cph-ac221@cphbusiness.dk>
2020-05-04 20:48:04 +0200

    Test til getDegree til FlatRoof er lavet og rettet så det kører

--- Projektskabelon-master/src/main/java/FunctionLayer/RoofFlat.java
+++ Projektskabelon-master/src/main/java/FunctionLayer/RoofFlat.java
@@ -5,23 +5,28 @@ import java.util.ArrayList;
 public class RoofFlat extends Roof {
 
     private ArrayList<Material> tagMaterialList;
-    private int roofTiltDegree;
-    private double roofRaisedCmPrCmLength = 3.0 / 100.0; //TODO - Final fordi det er en konstant?
+    private double roofRaisedCmPrCmLength = 3.0/100.0; //TODO - Final fordi det er en konstant?
+    private int roofTiltInMiliMeters = 3;
 
     public RoofFlat(int height, int length, int width) {
-        super(height, length, width, 0, false);
-        super.setDegree(roofTiltDegree);
+        //Math.round() fordi ellers vil det være 1 grad og den skal mindst være 3 cm pr 100cm i stigning hvilket svarer
+        // til 1,7... grader
+        super(height, length, width, (int) Math.round(Math.toDegrees(Math.atan(3.0 / 100.0))), false);
         tagMaterialList = new ArrayList();
     }
 
     //Beregner af fladt tags hældning i grader
     public int flatRoofDegreeCalculator() {
-        roofTiltDegree = (int) Math.atan(roofRaisedCmPrCmLength);
+        int roofTiltDegree = (int) Math.round(Math.toDegrees(Math.atan(roofRaisedCmPrCmLength))) ;
         return roofTiltDegree;
     }
 
     public double getRoofRaisedCmPrCmLength() {
         return roofRaisedCmPrCmLength;
+    }
+
+    public int getRoofTiltInMiliMeters() {
+        return roofTiltInMiliMeters;
     }
 
     @Override
