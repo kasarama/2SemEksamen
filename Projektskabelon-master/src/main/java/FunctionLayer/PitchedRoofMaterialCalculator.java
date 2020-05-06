eb966930541d96f58c712b971aed899d04e1ee6d
Mona Jakob Meshal <monajakobmeshal@Monas-MacBook.lan>
2020-05-07 00:46:34 +0200

    review and adjust PR Calculations

--- Projektskabelon-master/src/main/java/FunctionLayer/PitchedRoofMaterialCalculator.java
+++ Projektskabelon-master/src/main/java/FunctionLayer/PitchedRoofMaterialCalculator.java
@@ -1,66 +1,143 @@
 package FunctionLayer;
 
-public class PitchedRoofMaterialCalculator {
+public class PitchedRoofMaterialCalculator extends Roof {
+    //todo: fix attributes with attributes from Roof?
 
-    Construction construction;
-    private int length = construction.getCarportLength();
-    private int width = construction.getCarportWidth();
 
     private int roofLength;
-    private int vindskider;
+    private int roofWidth;
+    private int roofHeight;
+    private int vindskeder;
     private int vandbraet;
     private int stern;
+    private int toplaegteholder;
+    private int spaer;
     private int t1_SpaerLength = 540;  //taglægte til spær
     private int t1_RygstenLength = 420; //taglægte til rygsten
     private int tagfodslaegteBraet = 540; // tagfodslægteBræt
     private int tagplade; // B&C Dobbelt -s sort (tagplade)
 
-    //** Beregning af antal taglægter for pitchedRoof - Remember: tilpas med t1_SpaerLength! **
-    private int numberOfT1_Spaer (int roofLength)
+    //** Beregning af antal taglægter i forhold til tagets bredde - Remember: tilpas med t1_SpaerLength!? **
+    private int numberOfT1_Spaer (int roofWidth)
     {
         //length = roofLength;
         int T1_SpaerDistance = 307; // 307 mm mellem hvert lægte - dog ikke den første
         int topDistance = 30; // 30 mm på hver side dvs *2
 
-        int numberOfT1_Spaer = roofLength - (topDistance * 2)/T1_SpaerDistance + 2;
+        int numberOfT1_Spaer = roofWidth - (topDistance * 2)/T1_SpaerDistance + 2; // 2 = 350mm bræt
         return numberOfT1_Spaer;
     }
 
-    // ** Beregning af antal sternbrædder i forhold til tagets længde - stern skal have samme længde som taget **
-    //Note: antager at 30 mm udhæng er inkluderet i tagets længde.  --> max/min roofLength?
-    public static int numberOfstern (int roofLength)
+    // ** Beregning af antal sternbrædder i forhold til tagets længde - stern skal have samme længde som taget + 300 mm**
+    public static int numberOfStern (int roofLength)
     {
         int numberOfStern = 0;
-        if (roofLength <= 600 ) //600 mm = stern længde
-        {
-            numberOfStern = 2; // 1 on each side
-        }
-        else if (roofLength > 600)
+        int sternLength = roofLength + 300; //tag længde + 300mm lægte udhæng
+
+        if (roofLength <= 600 ) //600 mm = 1 stern længde - if roofLength equal/smaller than 600
         {
             numberOfStern = 4; // 2 on each side
+        }
+        else if (roofLength > 600) //if bigger than 600
+        {
+            numberOfStern = 6; // 4 on each side
+        }
+        else
+        {
+            numberOfStern = 8;
         }
 
         return numberOfStern;
     }
 
-    //Beregning af antal vindskider
-    /* Vindskeder på rejsningen monteres med skruer på lægte enderne husk at dette bræt skal slutte
-     1 cm. Over tagstenene.
-     --> tag rejsning = ?
-     --> tagsten højde = ?
-     */
+    //** Beregning af antal Toplægteholdere i forhold til spær (beslag) ** (OPS NOT MINE)
+    private static int numberOfToplaegteHolder (int toplaegteholder, int spaer)
+    {
+        int numberOfToplaegteHolder;
 
-    //Beregning af antal vandbræt
-    /*
-    På toppen af vindskeder i front og bagside monteres et vandbrædt (19x100) som beskyttelse og
-    overlap mellem tagsten og vindskede.
-     */
+        numberOfToplaegteHolder = toplaegteholder * spaer;
+        return numberOfToplaegteHolder;
+    }
 
-    //Beregning af.. the needed taglægte distance?
-    //Beregning af..topLægter Rest længde
-    //Beregning af tagplade amount = for loop in for loop. consider roof length and width + measurement of tagplade
+    //** Beregning af T1 toplægte (til rygsten) i forhold til tag længde **
+    public static int numberOfT1_RygstenLength(int roofLength)
+    {
+        //int toplaegteLength = 420; // 420 mm = 1 toplægte længde
+        int numberOfToplaegte = 0;
 
+        if(roofLength <= 840) // stk af 420 dvs *2= 840
+        {
+            numberOfToplaegte = 2;
+        }
+        else if (roofLength > 840)
+        {
+            numberOfToplaegte = 4;
+        }
+        else
+        {
+            numberOfToplaegte = 6;
+        }
+        return numberOfToplaegte;
+    }
 
-    //Rest calculations: sternbrædder, taglægte
+    //** Beregning af antal tagfodslægte i forhold til taget længde **
+    public static int numberOfTagfodsLaegte (int roofLength)
+    {
+        int numberOfTagfodsLaegte = 0;
+        if (roofLength <= 1620 ) //340 * 3stk ---> 340 mm = længde af 1 tagfodslægte
+        {
+            numberOfTagfodsLaegte = 3;
+        }
+        else if (roofLength > 1620)
+        {
+            numberOfTagfodsLaegte = 6;
+        }
+        else {
+            numberOfTagfodsLaegte = 9;
+        }
+        return numberOfTagfodsLaegte;
+    }
 
+    //** Beregning af antal vindskeder i forhold til tagets længde **
+    public static int numberOfVindskeder (int roofLength, int roofHeight)
+    {
+        int numberOfVindskeder = 0;
+        boolean isVindskeder;
+
+        if (roofHeight < 100) //if roofHeight is less than 1 m
+        {
+            isVindskeder = false; // no vindskeder needed
+            return 0;
+        }
+        else {  //roofHeight > 100
+            isVindskeder = true; //vinsdkeder is needed
+
+            if (roofLength <= 480 ) //if roofLength equal/smaller than 480 - længde af 1 vindskede bræt
+            {
+                numberOfVindskeder = 2;
+            }
+            else if (roofLength > 480 )
+            {
+                numberOfVindskeder = 4;
+            }
+        }
+        return numberOfVindskeder;
+    }
+
+    //** Beregning af antal vandbræt i forhold til antal vindskider ** - skal monteres på vindskider
+    //Note: antal vandbræt = antal vindskeder.
+    public static int numberOfVandbraet(int roofLength)
+    {
+        int numberOfVandbraet = 0;
+
+        if (roofLength <= 480 ) //if roofLength equal/smaller than 480 - længde af 1 vandbræt
+        {
+            numberOfVandbraet = 2;
+        }
+        else if (roofLength > 480 )
+        {
+            numberOfVandbraet= 4;
+        }
+        return numberOfVandbraet;
+    }
 }
