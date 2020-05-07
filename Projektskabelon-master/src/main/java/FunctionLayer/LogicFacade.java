02eaaaef5fea1e6220044fd0bc426fe2be56b656
Mia Simone <cph-md291@cphbusiness.dk>
2020-05-07 17:57:23 +0200

    Lidt html, lidt mapper

--- Projektskabelon-master/src/main/java/FunctionLayer/LogicFacade.java
+++ Projektskabelon-master/src/main/java/FunctionLayer/LogicFacade.java
@@ -21,8 +21,8 @@ public class LogicFacade {
         return user;
     }
 
-    public static Material getMaterialBySizeName(int size) throws LoginSampleException {
-        return MaterialMapper.getMaterialBySizeName(size);
+    public static Material getMaterialBySizeName(int size, String name) throws LoginSampleException {
+        return MaterialMapper.getMaterialBySizeName(size, name);
     }
     public static Material getMaterialByID(int id) throws LoginSampleException {
         return MaterialMapper.getMaterialByID(id);
