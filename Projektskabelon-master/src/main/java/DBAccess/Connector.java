6d8d28e1794bbaae0e22482401ba8a5904fa5689
Mia Simone <cph-md291@cphbusiness.dk>
2020-05-08 11:32:25 +0200

    connector

--- Projektskabelon-master/src/main/java/DBAccess/Connector.java
+++ Projektskabelon-master/src/main/java/DBAccess/Connector.java
@@ -1,16 +1,9 @@
 package DBAccess;
 
-import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.SQLOutput;
 
-/**
- * The purpose of Connector is to...
- *
- * @author kasper
- */
 public class Connector {
 
     private static String URL;
@@ -33,43 +26,11 @@ public class Connector {
     }
 
     public static void setDBCredentials() {
-        String[] data = readData();
-        String deployed = System.getenv("DEPLOYED");
-        if (deployed != null) {
-            // Prod: hent variabler fra setenv.sh i Tomcats bin folder
-            URL = System.getenv("JDBC_CONNECTION_STRING");
-            USERNAME = System.getenv("JDBC_USER");
-            PASSWORD = System.getenv("JDBC_PASSWORD");
-        } else {
-            // Localhost
-            URL = "jdbc:mysql://localhost:3306/fogDB?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
-            USERNAME = data[0];
-            PASSWORD = data[1];
-        }
-    }
-
-    public static String[] readData() {
-
-        String[] data = new String[2];
-
-        String path = "dbAccess/dbAccess.txt";
-
-
-        try {
-            FileReader fr = new FileReader(new File(path));
-            BufferedReader br = new BufferedReader(fr);
-            String line = br.readLine();
-            data = line.split(";");
-        }catch (FileNotFoundException ex){
-            System.out.println("File not found "+path);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-        }
-
-        return data;
+        // Prod: hent variabler fra setenv.sh i Tomcats bin folder
+        URL = "jdbc:mysql://localhost:3306/fogDB?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
+        USERNAME = "root";
+        PASSWORD = "Kat4Kanin5Hund1Hamster1";
 
     }
-
 
 }
