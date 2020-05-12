59ea8c588d7f9dabc1cd9d9ef2141cfbc0f0e0d0
Mia Simone <cph-md291@cphbusiness.dk>
2020-05-12 14:23:54 +0200

    fladt tag oppefra er done

--- Projektskabelon-master/src/main/java/PresentationLayer/Drawing.java
+++ Projektskabelon-master/src/main/java/PresentationLayer/Drawing.java
@@ -6,47 +6,29 @@ import FunctionLayer.Svg;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Drawing extends Command{
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Construction con = new Construction();
-        //int width = con.getConstructionWidth();
-        int widthActual = 7800;
-        // Width: 780, height: 600
-        con.setConstructionLength(7800);
-        con.setConstructionWidth(6000);
-        int width = (con.getConstructionLength()/10)/2;
-        int height = (con.getConstructionWidth()/10)/2;
-        //int height = con.getConstructionHeight();
-        String viewBox = "-100, 80, 900, 500";
-        Svg svgFromAbove = new Svg(780, 600, viewBox, 0, 0);
-            // Hvid baggrund:
-            svgFromAbove.addRect(-100,80, 1000, 900);
-            // Rem, spær, stolper, hulbånd, understern, overstern
-        // Baggrund:
-            //svgFromAbove.addRect(0,0,height,width);
-        // Spær:
-            svgFromAbove.addRect(0,0,600,3);
-            svgFromAbove.addRect(780-4,0,600,3);
-        // Remme:
-            svgFromAbove.addRect(0,30,3,780);
-            svgFromAbove.addRect(0,600-30,3,780);
-        // Hulbånd:
-        int hulbåndWidth = (((ConstructionSizeCalculator.postDistanceMax3000(widthActual)*3+100)/10)/3)*2;
-            svgFromAbove.addBand(7, 30,600, 570);
-            //svgFromAbove.addBand(0, height-30,height, 30);
+        //con.setConstructionLength(4200);
+        //con.setConstructionWidth(3000);
 
-        request.setAttribute("svgFromAbove", svgFromAbove.toString());
+        int width = (con.getConstructionLength()/10);
+        int height = (con.getConstructionWidth()/10);
+        
+        // Grundet tegningens størrelses, deles width og height med 2 så tegningen ikke bliver for stor
+        if (width>500 || height>500){
+            width = width/2;
+            height = height/2;
+        }
 
-
-
-        String viewBox2 = "-50, 0, " + (width+100) + ", " + (height+50);
+        String viewBox2 = "-50, -10, " + (width+100) + ", " + (height+50);
         Svg svg = new Svg(width+100, height+50, viewBox2, 0, 0);
+
         // Baggrund:
         svg.addRect(0,0,height,width);
+
         // Remme:
         svg.addRect(0,30,3,width);
         svg.addRect(0,height-30,3,width);
@@ -59,7 +41,7 @@ public class Drawing extends Command{
         int space = width/(ConstructionSizeCalculator.roofSpaerAmount(con));
         svg.addRect(0,0,height,3);
         // Der skal sættes et spær for hvert mellemrum: space, space*2, space*3, indtil man når antallet af stopler
-        for (int i = 0; i < ConstructionSizeCalculator.roofSpaerAmount(con)+1; i++){
+        for (int i = 0; i < ConstructionSizeCalculator.roofSpaerAmount(con); i++){
             svg.addRect(space*i, 0, height, 3);
         }
         svg.addRect(width,0,height,3);
@@ -75,8 +57,22 @@ public class Drawing extends Command{
         svg.addBand(0, 30, spacePosts, height-30);
         svg.addBand(0, height-28, spacePosts, 30);
         // Mål:
-        svg.addArrows(-25, 0, -25, height, -25, height/2, -90, height + " cm");
-        svg.addArrows(0, height+25, width, height+25, width/2,height+30, 0, width + " cm");
+        // width og height skal ganges med 2, da hvis de er over 500 så deles de længere oppe ad hensyn til størrelsen af tegningen
+        String text1 = "";
+        String text2 = "";
+        if (height>500){
+            text1 = height *2 + " cm";
+        } else {
+            text1 = height + " cm";
+        }
+        if (width>500){
+            text2 = width *2 + " cm";
+        } else {
+            text2 = width + " cm";
+        }
+        svg.addArrows(-25, 0, -25, height, -35, height/2, -90, text1);
+        svg.addArrows(0, height+25, width, height+25, width/2, height+40, 0, text2);
+
         request.setAttribute("svgdrawing", svg.toString());
         return "drawing";
     }
