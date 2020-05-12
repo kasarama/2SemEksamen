34bded59a568f8034a53c3529d20fe99938813e5
kasarama <magdalena.wawrzak@gmail.com>
2020-05-12 14:52:44 +0200

    work in progress

--- Projektskabelon-master/src/main/java/PresentationLayer/ShowEditedConstruction.java
+++ Projektskabelon-master/src/main/java/PresentationLayer/ShowEditedConstruction.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ShowEditedConstruction  extends Command{
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-         
+
         UpdateConstruction.validate(construction);
         return "itemList";
     }
